c220c277155afa0c95baa969ae57db57f354a9c8
Jin <suhjin700@gmail.com>
2023-10-22 17:57:07 +0900

    metadata 수정

--- app/(page)/about/page.tsx
+++ app/(page)/about/page.tsx
@@ -1,3 +1,9 @@
+import { URL } from '@/app/_utils/constants';
+
+export const metadata = {
+  title: URL.ABOUT.title,
+};
+
 export default function AboutPage() {
   return <div>About Page</div>;
 }
---
+++ app/_utils/constants/url.constants.ts
@@ -0,0 +1,10 @@
+export const URL = {
+  MAIN: {
+    path: '/',
+    title: 'Home',
+  },
+  ABOUT: {
+    path: '/about',
+    title: 'About',
+  },
+};
